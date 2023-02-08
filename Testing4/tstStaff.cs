3789fa1224b6bccc78630d74f3fea2ebff1b135c
Nk1756 <losblancos875@gmail.com>
2023-02-08 13:54:56 +0000

    Made some changes to Staff testing

---
+++ Testing4/tstStaff.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaff
+    {
+         
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staff = new clsStaff();
+            Assert.IsNotNull(staff);
+        }
+
+    }
+}
