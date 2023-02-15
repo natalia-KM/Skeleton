2db8e973e566182af8ad6a718301b4343c3e92d4
P2645147 <P2645147@JWHX0005WIN035W>
2023-02-15 11:10:48 +0000

    Added testing for Customer class

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer testCustomer = new clsCustomer();
+            Assert.IsNotNull(testCustomer);
+        }
+    }
+}
