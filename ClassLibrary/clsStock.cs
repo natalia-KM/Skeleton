124f2e5772f876481781be0aa24426978ccc91ab
P2658896 <p2658896@my365.dmu.ac.uk>
2023-02-15 10:11:35 +0000

    Creating the properties for the stock table design

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public static string Size { get; set; }
+        public static string Type { get; set; }
+        public static int Stock { get; set; }
+        public static double Price { get; set; }
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
