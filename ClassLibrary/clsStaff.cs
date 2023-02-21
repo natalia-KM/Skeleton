bd0be326d57f281b45329f0d4b11063e9c12f543
Nk1756 <losblancos875@gmail.com>
2023-02-21 00:32:37 +0000

    Fixed clsStaff and tstStaff

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public Int32 EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeePosition { get; set; }
+        public string EmployeeEmail { get; set; }
+        public DateTime EmployeeStartDate { get; set; }
+        public double EmployeeSalary { get; set; }
+        public bool BonusEligible { get; set; }
+    }
+}
