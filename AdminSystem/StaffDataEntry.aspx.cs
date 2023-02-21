77ddbb8b1ca05fee96b7659092c42362f36498bd
Nk1756 <losblancos875@gmail.com>
2023-02-21 01:22:34 +0000

    Added a Presentation Layer. Finished 1 element only

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    }
 
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsStaff staff = new clsStaff();
+        staff.EmployeeName = txtEmployeeName.Text;
+        Session["staff"] = staff;
+        Response.Redirect("StaffViewer.aspx");
     }
 }
--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsStaff staff = new clsStaff();
+        staff = (clsStaff)Session["staff"];
+        Response.Write(staff.EmployeeName);
     }
 }
