955137759c9a20105a4f1eebbde3c5124109ffba
jagree03 <jagree03@outlook.com>
2023-02-22 14:00:28 +0000

    Added basic presentation layer for Order (clsOrder)

--- AdminSystem/OrdersViewer.aspx.cs
+++ AdminSystem/OrdersViewer.aspx.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        // Retrieve the data from the session obj
+        AnOrder = (clsOrder) Session["AnOrder"];
+        // Display all data of the Order obj, all properties.
+        Response.Write("OrderID: " + AnOrder.OrderID + "<br>Order Date: " + AnOrder.OrderDate + "<br>Order Cost: " + AnOrder.OrderCost +
+            "<br>CustomerID: " + AnOrder.CustomerID + "<br>Notes: " + AnOrder.Notes + "<br>Order Quantity: " + AnOrder.OrderQuantity +
+            "<br>Dispatched: " + AnOrder.Dispatched);
+        
+        //Response.Write("OrderDate: " + AnOrder.OrderDate + " ");
+        //Response.Write("OrderCost: " + AnOrder.OrderCost + " ");
+        //Response.Write("CustomerID: " + AnOrder.CustomerID + " ");
+        //Response.Write("Notes: " + AnOrder.Notes + " ");
+        //Response.Write("OrderQuantity: " + AnOrder.OrderQuantity + " ");
+        //Response.Write("Dispatched: " + AnOrder.Dispatched + " ");
+ 
     }
 }
