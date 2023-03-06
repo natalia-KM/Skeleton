01441fce5dd8e4169b688caaca9d786a135f1cfe
Nk1756 <losblancos875@gmail.com>
2023-03-06 14:42:47 +0000

    Added Find Method (Staff)

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,108 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public Int32 EmployeeID { get; set; }
-        public string EmployeeName { get; set; }
-        public string EmployeePosition { get; set; }
-        public string EmployeeEmail { get; set; }
-        public DateTime EmployeeStartDate { get; set; }
-        public double EmployeeSalary { get; set; }
-        public bool BonusEligible { get; set; }
+
+        private Int32 mEmpID;
+        public Int32 EmployeeID
+        {
+            get
+            {
+                return mEmpID;
+            }
+            set
+            {
+                mEmpID = value;
+            }
+        }
+
+        private string mEmpName;
+        public string EmployeeName
+        {
+            get
+            {
+                return mEmpName;
+            }
+            set
+            {
+                mEmpName = value;
+            }
+        }
+
+        private string mEmpPosition;
+        public string EmployeePosition
+        {
+            get
+            {
+                return mEmpPosition;
+            }
+            set
+            {
+                mEmpPosition = value;
+            }
+        }
+
+        private string mEmpEmail;
+        public string EmployeeEmail
+        {
+            get
+            {
+                return mEmpEmail;
+            }
+            set
+            {
+                mEmpEmail = value;
+            }
+        }
+
+        private DateTime mEmpStartDate;
+        public DateTime EmployeeStartDate
+        {
+            get
+            {
+                return mEmpStartDate;
+            }
+            set
+            {
+                mEmpStartDate = value;
+            }
+        }
+
+        private double mEmpSalary;
+        public double EmployeeSalary
+        {
+            get
+            {
+                return mEmpSalary;
+            }
+            set
+            {
+                mEmpSalary = value;
+            }
+        }
+
+        private bool mBonusE;
+        public bool BonusEligible
+        {
+            get
+            {
+                return mBonusE;
+            }
+            set
+            {
+                mBonusE = value;
+            }
+        }
+
+        public bool Find(Int32 empID)
+        {
+            mEmpID = 1;
+            mEmpName = "Natalia";
+            mEmpPosition = "Manager";
+            mEmpEmail = "nat@gmail.com";
+            mEmpStartDate = Convert.ToDateTime("05/02/2023");
+            mEmpSalary = 23000.0;
+            mBonusE = false;
+            return true;
+        }
     }
 }
