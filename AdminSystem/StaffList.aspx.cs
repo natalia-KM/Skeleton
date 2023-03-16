6f20f45f4cd21a1543765d13e68174521d58db10
Nk1756 <losblancos875@gmail.com>
2023-03-16 14:51:55 +0000

    Created Collection Class Properties - Staff

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(IsPostBack == false)
+        {
+            DisplayStaff();
+        }
+    }
 
+    void DisplayStaff()
+    {
+        clsStaffCollection allStaff = new clsStaffCollection();
+        lstStaffList.DataSource = allStaff.StaffList;
+        lstStaffList.DataValueField = "EmployeeID"; //set the name of primary key
+        lstStaffList.DataTextField = "EmployeeName"; // set data field to display
+        lstStaffList.DataBind();
     }
 }
---
+++ Testing4/tstStaffCollection.cs
@@ -0,0 +1,88 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaffCollection allStaff = new clsStaffCollection();
+            Assert.IsNotNull(allStaff);
+        }
+
+        [TestMethod]
+        public void StaffListOK()
+        {
+            clsStaffCollection allStaff = new clsStaffCollection();
+
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff staff = new clsStaff();
+
+            staff.BonusEligible = true;
+            staff.EmployeeID = 1;
+            staff.EmployeeName = "Soto";
+            staff.EmployeePosition = "Manager";
+            staff.EmployeeEmail = "soto@twentythree.com";
+            staff.EmployeeSalary = 23000.0;
+            staff.EmployeeStartDate = DateTime.Parse("23/02/2023");
+
+            TestList.Add(staff);
+
+            allStaff.StaffList = TestList;
+
+            Assert.AreEqual(allStaff.StaffList, TestList);
+        }
+
+     
+
+        [TestMethod]
+        public void ThisEmployeePropertyOK()
+        {
+            clsStaffCollection allStaff = new clsStaffCollection();
+
+            clsStaff staff = new clsStaff();
+
+            staff.BonusEligible = true;
+            staff.EmployeeID = 1;
+            staff.EmployeeName = "Soto";
+            staff.EmployeePosition = "Manager";
+            staff.EmployeeEmail = "soto@twentythree.com";
+            staff.EmployeeSalary = 23000.0;
+            staff.EmployeeStartDate = DateTime.Parse("23/02/2023");
+
+
+            allStaff.ThisEmployee = staff;
+
+            Assert.AreEqual(allStaff.ThisEmployee, staff);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsStaffCollection allStaff = new clsStaffCollection();
+
+            List<clsStaff> TestList = new List<clsStaff>();
+            clsStaff staff = new clsStaff();
+
+            staff.BonusEligible = true;
+            staff.EmployeeID = 1;
+            staff.EmployeeName = "Soto";
+            staff.EmployeePosition = "Manager";
+            staff.EmployeeEmail = "soto@twentythree.com";
+            staff.EmployeeSalary = 23000.0;
+            staff.EmployeeStartDate = DateTime.Parse("23/02/2023");
+
+            TestList.Add(staff);
+
+            allStaff.StaffList = TestList;
+
+            Assert.AreEqual(allStaff.Count, TestList.Count);
+        }
+
+    }
+}
