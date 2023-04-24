b7159a76bc25fa7f36954611ce48241aca9cc0d4
P2645147 <P2645147@JWHX0005WIN035W>
2023-04-24 21:06:35 +0100

    Created collection class and properties for Customer component

---
+++ ClassLibrary/clsCustomerCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsCustomerCollection
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        public List<clsCustomer> CustomerList
+        {
+            get
+            {
+                return mCustomerList;
+            }
+            set
+            {
+                mCustomerList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mCustomerList.Count;
+            }
+            set
+            {
+                // we shall worry about this later
+            }
+        }
+        public clsCustomer ThisCustomer { get; set; }
+
+        public clsCustomerCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsCustomer ACustomer = new clsCustomer();
+                ACustomer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                ACustomer.FullName = Convert.ToString(DB.DataTable.Rows[Index]["FullName"]);
+                ACustomer.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                ACustomer.PhoneNumber = Convert.ToString(DB.DataTable.Rows[Index]["PhoneNumber"]);
+                ACustomer.DateOfBirth = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateOfBirth"]);
+                ACustomer.TotalSpent = Convert.ToDouble(DB.DataTable.Rows[Index]["TotalSpent"]);
+                ACustomer.PaymentDataAdded = Convert.ToBoolean(DB.DataTable.Rows[Index]["PaymentDataAdded"]);
+                mCustomerList.Add(ACustomer);
+                Index++;
+            }
+        }
+    }
+}
---
+++ Testing3/tstCustomerCollection.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomer);
+        }
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.PaymentDataAdded = true;
+            TestItem.CustomerID = 20;
+            TestItem.DateOfBirth = DateTime.Now.Date;
+            TestItem.FullName = " customer full name ";
+            TestItem.Email = " customer email ";
+            TestItem.PhoneNumber = " customer phone ";
+            TestItem.TotalSpent = 1000000;
+            TestList.Add(TestItem);
+            AllCustomer.CustomerList = TestList;
+            Assert.AreEqual(AllCustomer.CustomerList, TestList);
+        }
+        
+        [TestMethod]
+        public void ThisCustomerPropertyOK()
+        {
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            clsCustomer TestCustomer = new clsCustomer();
+            TestCustomer.PaymentDataAdded = true;
+            TestCustomer.CustomerID = 20;
+            TestCustomer.DateOfBirth = DateTime.Now.Date;
+            TestCustomer.FullName = " customer full name ";
+            TestCustomer.Email = " customer email ";
+            TestCustomer.PhoneNumber = " customer phone ";
+            TestCustomer.TotalSpent = 1000000;
+            AllCustomer.ThisCustomer = TestCustomer;
+            Assert.AreEqual(AllCustomer.ThisCustomer, TestCustomer);
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomer = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.PaymentDataAdded = true;
+            TestItem.CustomerID = 20;
+            TestItem.DateOfBirth = DateTime.Now.Date;
+            TestItem.FullName = " customer full name ";
+            TestItem.Email = " customer email ";
+            TestItem.PhoneNumber = " customer phone ";
+            TestItem.TotalSpent = 1000000;
+            TestList.Add(TestItem);
+            AllCustomer.CustomerList = TestList;
+            Assert.AreEqual(AllCustomer.Count, TestList.Count);
+        }
+        
+
+    }
+}
